2b8c9822302a8891934ab68534b0176b08c166c4
JesseNovice <jesseroylees@gmail.com>
2024-04-11 09:59:45 +1200

    event handler and API search

---
+++ src/components/video_list.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const VideoList = () => {
+    return (
+        <ul className="col-md-4"> 
+        
+        </ul>
+    );
+};
+
+// col-md-4 this sets it as a column with 4 columns
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import youtubeApiSearch from "youtube-api-search";
 import API_Key from "./API_key";
 import SearchBar from "./components/search_bar";
-
+import VideoList from "./components/video_list";
 const Key = API_Key
 
+
 // Create a new compnent. This componebt should produce some HTML
-const App = () => {
+class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = { videos: [] };
+        // API Search 
+        youtubeApiSearch({key: Key, term: 'surfboards'}, (videos) => {
+            this.setState({videos}); // because the key value pair is the same name it is resolved as this.setState({videos: videos});
+        });
+    }
+
+    render() {
     return (
     <div>
         <SearchBar />
+        <VideoList videos={this.state.videos}/>
     </div>
     );
+    }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM).
